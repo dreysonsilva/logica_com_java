833c8787351b534e92965d879d039e0e4363be63
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-04 11:40:52 -0300

    informe nome e salário do funcionario

---
+++ src/NomeSalarioFuncionario.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class NomeSalarioDuncionario {
+	public static void main(String[] args) {
+	/**
+	 3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+	final uma mensagem.
+	Ex:
+	Nome do Funcionário: Maria do Carmo
+	Salário: 1850,45
+	O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+	*/
+		
+		String nome = JOptionPane.showInputDialog("Informe o nome do funcionario");
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("Informe o salário"));
+		
+		System.out.println("Nome do Funcionario(a): "+nome+"\nSalário: "+salario+"\nO/A funcionario(a) "+nome+" tem o salário de "+salario+" em junho.");
+	}
+}
