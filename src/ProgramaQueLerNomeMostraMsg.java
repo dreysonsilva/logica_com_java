00701ca29537b2719b35847dc54edffaa9720a48
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-04 00:49:35 -0300

    programa que ler nome e mostra mensagem boas vidas

---
+++ src/ProgramaQueLerNomeMostraMsg.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class ProgramaQueLerNomeMostraMsg {
+	public static void main(String[] args) {
+		/* Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+			 Ex:
+			 Qual é o seu nome? João da Silva
+			 Olá João da Silva, é um prazer te conhecer!
+		 */
+		
+		String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+		
+		System.out.println("Olá "+nome+ ", é um prazer te conhecer!");
+	}
+}
