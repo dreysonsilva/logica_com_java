23871a39a33935a513b9ba9cde4ec71f1427ec8e
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-08 12:47:33 -0300

    correção adicionado '2)' no comentário

--- src/ProgramaQueLerNomeMostraMsg.java
+++ src/ProgramaQueLerNomeMostraMsg.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class ProgramaQueLerNomeMostraMsg {
 	public static void main(String[] args) {
-		/* Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+		/* 2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
 			 Ex:
 			 Qual é o seu nome? João da Silva
 			 Olá João da Silva, é um prazer te conhecer!
