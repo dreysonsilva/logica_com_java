973f527e7f67050564d98d9ef8196eb5b4a17d16
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-08 12:46:02 -0300

    Comentário

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
-
 public class HelloWorld {
 	public static void main(String[] args) {
+		//1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+
 		System.out.println("Hello World");
 	}
 }
