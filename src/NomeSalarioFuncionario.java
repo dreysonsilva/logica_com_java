3c249fa588f84055d1e89598b0aa52a5e96a6220
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-08 07:46:10 -0300

    Correção de Duncionario para Funcionario - nome da classe

--- src/NomeSalarioFuncionario.java
+++ src/NomeSalarioFuncionario.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class NomeSalarioDuncionario {
+public class NomeSalarioFuncionario {
 	public static void main(String[] args) {
 	/**
 	 3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no
