d01857b4c8e7d5d3855581b45ccb6ea6f73895a4
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-09 22:33:43 -0300

    Ler uma distância em metros, mostrando os valores em outras medidas.

---
+++ src/LerUmaDistanciaEmMetros.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LerUmaDistanciaEmMetros {
+
+	public static void main(String[] args) {
+		/*
+		 * 8) Desenvolva um programa que leia uma distância em metros e mostre os valores
+			  relativos em outras medidas.
+			  Ex:
+		   	  Digite uma distância em metros: 185.72
+			  A distância de 85.7m corresponde a:
+			  0.18572Km
+			  1.8572Hm
+			  18.572Dam
+			  1857.2dm
+			  18572.0cm
+			  185720.0mm
+		 * */
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite uma distância em metros: ");
+		double distancia = Double.parseDouble(scan.next());
+		
+		System.out.println(distancia*0.001 +" km");
+		System.out.println(distancia*0.01 +" hn");
+		System.out.println(distancia*0.1 +" dam");
+		System.out.println(distancia*100 +" cm");
+		System.out.println(distancia*1000 +" mm");
+	}
+
+}
