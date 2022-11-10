38d99b30aa61ee64c8d8a2ea600675c575d09ddf
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-10 05:19:13 -0300

    Converte Dinheiro Carteira Para Dolar
    Math.floor - Este metódo está arrendondando para baixo

---
+++ src/ConverteDinheiroCarteiraParaDolar.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class ConverteDinheiroCarteiraParaDolar {
+
+	public static void main(String[] args) {
+		/*
+		 * 9) Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$)
+		 * e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45. 
+		 * */
+		double valorEmDolar = 3.45;
+		double qtdDinheiroCarteira =  Double.parseDouble(JOptionPane.showInputDialog("Informe quanto você tem em sua carteira"));
+		
+		double valorConvertidoDolar = qtdDinheiroCarteira  /valorEmDolar;
+		/**
+		 * Math.floor - Este metódo está arrendondando para baixo
+		 */
+		JOptionPane.showMessageDialog(null,"U$ "+ Math.floor(valorConvertidoDolar));
+	}
+
+}
