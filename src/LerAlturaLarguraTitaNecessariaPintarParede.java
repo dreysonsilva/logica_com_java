8de967d5af27afe353e0039babc01a6175698c35
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-14 08:46:42 -0300

    Ler altura e largura saber qtd tintas p pintura

---
+++ src/LerAlturaLarguraTitaNecessariaPintarParede.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class LerAlturaLarguraTitaNecessariaPintarParede {
+
+	public static void main(String[] args) {
+		/*
+		 * 10) Faça um algoritmo que leia a largura e altura de uma parede,
+		 *  calcule e mostre a área a ser pintada e a
+		 *  quantidade de tinta necessária para o serviço,
+		 *	sabendo que cada litro de tinta pinta uma área de 2metros quadrados 
+		 **/
+		
+		
+		//ENTRADA DE DADOS
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Altura?");
+		int altura = Integer.parseInt(scanner.next());
+		
+		System.out.println("Largura?");
+		int largura = Integer.parseInt(scanner.next());
+		
+		
+		//PROCESSAMENTO
+		int totalAlturaLargura = altura*largura;
+		int qtdTintas = 0;
+		
+		if(totalAlturaLargura % 2==1) {
+			qtdTintas = 1;
+		}else {
+			qtdTintas = 0;
+		}
+			
+		for(int i=1;i<=totalAlturaLargura;i++) {
+			if(i%2==0) {
+				qtdTintas++;
+			}
+		}
+			
+		//SAÍDA DE DADOS
+		System.out.println("Área a ser pintada: "+altura*largura);
+		System.out.println("Quantidade de tinta necessária para o serviço: "+qtdTintas);
+	}
+
+}
