94992d394b19e00feb88c454e83ab3cbab3cca2b
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-15 20:20:55 -0300

    Redução de tempo fumante(inicio)

---
+++ src/ReducaoTempoFumante.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReducaoTempoFumante {
+
+	public static void main(String[] args) {
+		/*16) [DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um
+		fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele
+		já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule
+		quantos dias de vida um fumante perderá e exiba o total em dias.*/
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Quantos cigarros fumado por dia?");
+		int qtdCigarrosPorDia = scanner.nextInt();
+		
+		System.out.println("Quanto tempo você fuma");
+		int qtdAnosTempoFumando = scanner.nextInt();
+		
+		System.err.println(qtdCigarrosPorDia);
+		System.out.println(qtdAnosTempoFumando);
+		
+
+		
+		
+
+	}
+
+}
