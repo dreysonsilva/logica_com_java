1b95ca05e09a2656c51ea71383799f96c48c46ec
Dreyson Santiago <dreysoninformar@gmail.com>
2022-11-24 06:19:37 -0300

    foi adicionado o if para processar o tempo de vida do fumante

--- src/ReducaoTempoFumante.java
+++ src/ReducaoTempoFumante.java
@@ -5,8 +5,8 @@ public class ReducaoTempoFumante {
 	public static void main(String[] args) {
 		/*16) [DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um
 		fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele
-		já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule
-		quantos dias de vida um fumante perderá e exiba o total em dias.*/
+		já fumou. Considere que um fumante perde 10 min de vida a cada cigarro.
+		Calcule quantos dias de vida um fumante perderá e exiba o total em dias.*/
 		
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
@@ -17,9 +17,14 @@ public class ReducaoTempoFumante {
 		System.out.println("Quanto tempo você fuma");
 		int qtdAnosTempoFumando = scanner.nextInt();
 		
-		System.err.println(qtdCigarrosPorDia);
-		System.out.println(qtdAnosTempoFumando);
-
+		int mes=12;
+		
+		
+		if(qtdAnosTempoFumando>0) {	
+			int reduçãoTempoVida = (qtdAnosTempoFumando*mes)*30;
+			int convetendoDiasEmHoras = reduçãoTempoVida * 24;
+			System.out.println("reduçãoTempoVida: "+(reduçãoTempoVida/24)*60);
+			System.out.println("convetendoDiasEmHoras: "+convetendoDiasEmHoras);
+		} 
 	}
-
 }
