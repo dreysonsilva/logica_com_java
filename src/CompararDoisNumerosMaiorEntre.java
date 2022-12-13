9274d674c4d45fcc461c01b5a4949924922a0dc5
Dreyson Santiago <dreysoninformar@gmail.com>
2022-12-13 08:01:50 -0300

    comparar dois numeros e verificar o maior deles

---
+++ src/CompararDoisNumerosMaiorEntre.java
@@ -0,0 +1,23 @@
+
+public class CompararDoisNumerosMaiorEntre {
+
+	public static void main(String[] args) {
+		/*26) Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando
+			  na tela uma das mensagens abaixo:
+			  - O primeiro valor é o maior
+		 	  - O segundo valor é o maior
+			  - Não existe valor maior, os dois são iguais
+		 */
+		
+		double n1 = 10;
+		double n2 = 10;
+		
+		if(n1>n2) {
+			System.out.println("O primeiro valor é o maior");
+		}else if(n2>n1) {
+			System.out.println("O segundo valor é o maior");
+		}else {
+			System.out.println("Não existe valor maior, os dois são iguais");
+		}
+	}
+}
